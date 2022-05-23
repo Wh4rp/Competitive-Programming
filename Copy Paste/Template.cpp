ec50ae64752e33ca5fad6b1efe196bcbab056ae5
Wh4rp <matias.fernandez@uc.cl>
2022-05-23 14:25:09 -0400

    Change Directory Name.

---
+++ Copy Paste/Template.cpp
@@ -0,0 +1,17 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define rep_(i, k, n) for (int i = k; i < n; ++i)
+using ll = long long;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.setf(ios::fixed);
+  cout.precision(10);
+  
+  
+  
+  return 0;
+}
