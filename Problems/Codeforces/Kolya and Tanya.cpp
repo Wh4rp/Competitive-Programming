bf8dab76a80ff4e2e7a7373821fb88d30f563ff9
Warp <matiasfernandeztaipe@gmail.com>
2022-06-18 21:08:53 -0400

    fixed problem

--- Problems/Codeforces/Kolya and Tanya.cpp
+++ Problems/Codeforces/Kolya and Tanya.cpp
@@ -3,39 +3,8 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < n; ++i)
 #define rep_(i, k, n) for (int i = k; i < n; ++i)
-#define loop(h) for (int hi = 0; hi < h; hi++)
-#define ii pair<int, int>
 using ll = long long;
 
-template <typename First>
-void read(First& t) {
-  cin >> t;
-}
-template <typename First, typename... Args>
-void read(First& f, Args&... args) {
-  cin >> f;
-  read(forward<Args&>(args)...);
-}
-
-// VARIADIC OUTPUT
-template <typename T>
-void print(T&& t) {
-  cout << t << "\n";
-}
-template <typename First, typename... Args>
-void print(First&& f, Args&&... args) {
-  cout << f << " ";
-  print(forward<Args&&>(args)...);
-}
-template <typename T>
-void printLn(T&& t) {
-  cout << t << "\n";
-}
-template <typename First, typename... Args>
-void printLn(First&& f, Args&&... args) {
-  cout << f << "\n";
-  printLn(forward<Args&&>(args)...);
-}
 ll m = 1000000007;
 long long binpow(long long a, long long b) {
   a %= m;
@@ -54,8 +23,10 @@ int main() {
   cin.tie(nullptr);
   cout.setf(ios::fixed);
   cout.precision(10);
+  
   ll n;
-  read(n);
-  print((binpow(27, n) - binpow(7, n)) % m);
+  cin >> n;
+  cout << (m + binpow(27, n) - binpow(7, n)) % m << '\n';
+  
   return 0;
 }
