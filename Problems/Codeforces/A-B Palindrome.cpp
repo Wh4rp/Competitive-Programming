4d7139d63b12084676fb99dfb0fc5034e67587e0
Warp <matiasfernandeztaipe@gmail.com>
2022-06-19 01:17:41 -0400

    imprved solution

--- Problems/Codeforces/A-B Palindrome.cpp
+++ Problems/Codeforces/A-B Palindrome.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < n; ++i)
 #define rep_(i, k, n) for (int i = k; i < n; ++i)
 using ll = long long;
- 
+
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
