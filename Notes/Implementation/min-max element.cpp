69b46b1f56a84978542e0ddb9029cf0e85c2276d
Warp <matiasfernandeztaipe@gmail.com>
2022-06-19 01:40:18 -0400

    update

--- Notes/Implementation/min-max element.cpp
+++ Notes/Implementation/min-max element.cpp
@@ -6,8 +6,8 @@ int v[10] = {4, 5, 6, 7, 8, 9, 1, 2, 3, 0};
 auto pointer_min = min_element(v, v + 10); // returns pointer to the first element of the min-element
 auto pointer_max = max_element(v, v + 10); // returns pointer to the first element of the max-element
 
-int index_min = pointer_min - v; // returns the index of the min-element
-int index_max = pointer_max - v; // returns the index of the max-element
+int index_min = min_element(v, v + 10) - v; // returns the index of the min-element
+int index_max = max_element(v, v + 10) - v; // returns the index of the max-element
 
-int value_min = *pointer_min; // returns the value of the min-element
-int value_max = *pointer_max; // returns the value of the max-element
+int value_min = *min_element(v, v + 10); // returns the value of the min-element
+int value_max = *max_element(v, v + 10); // returns the value of the max-element
