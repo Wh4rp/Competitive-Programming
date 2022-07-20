bc07cfe84fb232b64e597ccba056719ad936f669
warp <matiasfernandeztaipe@gmail.com>
2022-07-19 22:09:37 -0400

    added problem

---
+++ Problems/binarysearch/Invert Tree.py
@@ -0,0 +1,14 @@
+# class Tree:
+#     def __init__(self, val, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def solve(self, root):
+        if root:
+            root.left, root.right = root.right, root.left
+            self.solve(root.left)
+            self.solve(root.right)
+            return root
+        else:
+            return
