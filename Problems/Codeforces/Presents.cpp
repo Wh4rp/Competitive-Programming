8c8c9da1086da51dae0a80f5efe8ed2ddea20892
Wh4rp <matias.fernandez@uc.cl>
2023-03-21 12:53:19 -0300

    added problems

---
+++ Problems/Codeforces/Presents.cpp
@@ -0,0 +1,26 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define rep_(i, k, n) for (int i = k; i < n; ++i)
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.setf(ios::fixed);
+    cout.precision(10);
+
+    int n;
+    cin >> n;
+    int v[n + 1];
+    rep_(i, 1, n + 1) {
+        int aux;
+        cin >> aux;
+        v[aux] = i;
+    }
+    rep_(i, 1, n + 1) cout << v[i] << ' ';
+    cout << "\n";
+
+    return 0;
+}
