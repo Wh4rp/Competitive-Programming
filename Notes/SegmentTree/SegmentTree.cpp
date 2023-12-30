37e74fc150e602ea500b590f896e6dced2bc396b
Wh4rp <matiasfernandeztaipe@gmail.com>
2023-12-29 23:23:37 -0300

    Add SegmentTree implementation and La torre de la maldad problem solution

---
+++ Notes/SegmentTree/SegmentTree.cpp
@@ -0,0 +1,38 @@
+#include "../Template.cpp"
+
+struct Node
+{
+    int v;
+    Node() { v = 0; } // neutro
+    Node(int v) : v(v) {} // Node a(10); -> a.v := 10
+    Node(const Node &a, const Node &b) { v = a.v + b.v; }
+};
+
+// 0 - indexed / inclusive - exclusive
+// ST query(0, 10) -> [0, 10)
+template <class node>
+struct ST
+{
+    vector<node> t; int n;
+
+    ST(vector<node> &arr, int N) : n(N), t(N * 2)
+    {
+        copy(arr.begin(), arr.end(), t.begin() + n);
+        for (int i = n - 1; i > 0; --i) t[i] = node(t[i << 1], t[i << 1 | 1]);
+    }
+    void set(int p, const node &value)
+    {
+        for (t[p += n] = value; p >>= 1;)
+            t[p] = node(t[p << 1], t[p << 1 | 1]);
+    }
+    node query(int l, int r)
+    {
+        node ansl, ansr;
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1)
+        {
+            if (l & 1) ansl = node(ansl, t[l++]);
+            if (r & 1) ansr = node(t[--r], ansr);
+        }
+        return node(ansl, ansr);
+    }
+};
